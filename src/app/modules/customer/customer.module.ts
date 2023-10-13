ac7e1a7f3a407a12c137c1666d2adb52703588a2
112748_Ordoñez_David <112748@tecnicatura.frc.utn.edu.ar>
2023-10-13 13:09:01 -0300

    Register employee progress

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './components/home/home.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EmployeeRegistrationComponent } from './components/employee-registration/employee-registration.component';
+import { FormsModule, NgForm } from '@angular/forms';
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, EmployeeRegistrationComponent],
   providers: [],
-  imports: [CommonModule],
+  imports: [CommonModule, NgbModule, FormsModule],
   exports: [HomeComponent],
 })
 export class CustomerModule {}
