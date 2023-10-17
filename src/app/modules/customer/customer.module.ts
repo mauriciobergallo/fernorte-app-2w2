81b2bc733683bec9c3563fbac93cac0c363bd798
Alex <114224@tecnicatura.frc.utn.edu.ar>
2023-10-16 21:20:22 -0300

    Create Turner

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeComponent } from './components/home/home.component';
+import { TurnsComponentComponent } from './components/turns-component/turns-component.component';
+import { BtnNoCustomerComponent } from './components/btn-no-customer/btn-no-customer.component';
+import { BtnCustomerComponent } from './components/btn-customer/btn-customer.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TurnsComponentComponent, BtnNoCustomerComponent, BtnCustomerComponent],
   providers: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    ReactiveFormsModule
+  ],
   exports: [HomeComponent],
 })
 export class CustomerModule {}
---
+++ src/app/modules/customer/models/turn-response.spec.ts
@@ -0,0 +1,7 @@
+import { TurnResponse } from './turn-response';
+
+describe('TurnResponse', () => {
+  it('should create an instance', () => {
+    expect(new TurnResponse()).toBeTruthy();
+  });
+});
