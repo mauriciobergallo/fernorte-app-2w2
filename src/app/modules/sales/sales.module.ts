9fe0a046bb714cfc0e8b5f9fcc8c7d6590945288
ILPrado <pradoignaciolucas@gmail.com>
2023-10-18 18:27:33 -0300

    modify, folder home to folder order_sale
    Movi el codigo proporcionado por Medina Ezequiel de home a el componente order-sale para poder utilizar el componente home como la pantalla principal de ventas para redirigir a los distintos componentes una vez loggeado

--- src/app/modules/sales/sales.module.ts
+++ src/app/modules/sales/sales.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { SaleOrderProvider } from './providers/SaleOrderProvider';
 import { ProductProvider } from './providers/productProvider';
+import { SaleOrderComponent } from './components/sale_order/sale-order.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,SaleOrderComponent],
   providers: [SaleOrderProvider,ProductProvider],
   imports: [CommonModule],
-  exports: [HomeComponent],
+  exports: [HomeComponent,SaleOrderComponent],
 })
 export class SalesModule {}
