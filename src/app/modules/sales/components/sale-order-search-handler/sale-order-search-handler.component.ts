93189bb63e9d2951fb84673d868a200b6ed1b4db
113926-Macarena-Caridad <113926@tecnicatura.frc.utn.edu.ar>
2023-10-19 12:45:16 -0300

    components created

---
+++ src/app/modules/sales/components/sale-order-search-handler/sale-order-search-handler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fn-sale-order-search-handler',
+  templateUrl: './sale-order-search-handler.component.html',
+  styleUrls: ['./sale-order-search-handler.component.css']
+})
+export class SaleOrderSearchHandlerComponent {
+
+}
