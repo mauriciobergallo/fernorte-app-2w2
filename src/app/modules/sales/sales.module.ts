4b12f7a456925044510d3c635df5bd080b1c61cd
113904_SENESTRARI_NICOLAS <113904@tecnicatura.frc.utn.edu.ar>
2023-10-19 12:02:55 -0300

    window of create-payment

--- src/app/modules/sales/sales.module.ts
+++ src/app/modules/sales/sales.module.ts
@@ -7,9 +7,11 @@ import { ProductProvider } from './providers/productProvider';
 import { SaleOrderComponent } from './components/sale_order/sale-order.component';
 import { FormsModule } from '@angular/forms';
 import { CalcularTotalPipe } from './pipes/calcular-total.pipe';
+import { CreatePaymentComponent } from './components/create-payment/create-payment.component';
+import { ViewPaymentComponent } from './components/view-payment/view-payment.component';
 
 @NgModule({
-  declarations: [HomeComponent,SaleOrderComponent, CalcularTotalPipe],
+  declarations: [HomeComponent,SaleOrderComponent, CalcularTotalPipe, CreatePaymentComponent, ViewPaymentComponent],
   providers: [SaleOrderProvider,ProductProvider],
   imports: [CommonModule,FormsModule],
   exports: [HomeComponent,SaleOrderComponent],
