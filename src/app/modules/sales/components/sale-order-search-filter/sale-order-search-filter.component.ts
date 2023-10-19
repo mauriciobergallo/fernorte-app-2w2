15247b40ac065433a260bced0849f726c5a9d2ec
113926-Macarena-Caridad <113926@tecnicatura.frc.utn.edu.ar>
2023-10-19 17:18:59 -0300

    filter component

--- src/app/modules/sales/components/sale-order-search-filter/sale-order-search-filter.component.ts
+++ src/app/modules/sales/components/sale-order-search-filter/sale-order-search-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'fn-sale-order-search-filter',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./sale-order-search-filter.component.css']
 })
 export class SaleOrderSearchFilterComponent {
+  @Output() filter:EventEmitter<string> = new EventEmitter();
+  nOrder:string="";
+  fromDate:string="";
+  toDate:string="";
+  doc:string="";
+  dates:string="";
 
+
+  onSendNOrder(form : NgForm){
+    this.nOrder = form.value.nOrder
+    this.onSendFilter(form, this.nOrder)
+  }
+
+  onSendDoc(form : NgForm){
+    this.doc = form.value.doc
+    this.onSendFilter(form, this.doc)
+  }
+
+  onSendDates(form: NgForm){
+    this.dates=this.fromDate+"-"+this.toDate
+    this.onSendFilter(form, this.dates)
+  }
+
+  onSendFilter(form : NgForm, filter:string){
+    if(form.valid){
+      console.log(filter)
+      this.filter.emit(filter);
+    }
+  }
+  
 }
