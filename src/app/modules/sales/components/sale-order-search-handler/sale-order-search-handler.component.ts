42b02b152a337a279aae92992f85083b0cd0e52a
113926-Macarena-Caridad <113926@tecnicatura.frc.utn.edu.ar>
2023-10-20 14:08:36 -0300

    comunication with handler

--- src/app/modules/sales/components/sale-order-search-handler/sale-order-search-handler.component.ts
+++ src/app/modules/sales/components/sale-order-search-handler/sale-order-search-handler.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./sale-order-search-handler.component.css']
 })
 export class SaleOrderSearchHandlerComponent {
+  filter:string="";
+  fromDate:string="";
+  toDate:string="";
 
+  onReceiveFilter(filterSent:string){
+    console.log(this.filter)
+    this.filter=filterSent;
+  }
+  
+  onSeparateFilter(filter:string){
+    if(this.filter.includes('-')){
+      const index = this.filter.indexOf('/')
+        this.fromDate = this.filter.slice(0,index)
+        this.toDate = this.filter.slice(index, this.filter.length)
+    }
+  }
 }
