6045e07050bc59acd8ea8250e918cf5ba9284942
Alex <114224@tecnicatura.frc.utn.edu.ar>
2023-10-23 00:33:50 -0300

    Add Modal of NewUser

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { UserRootComponent } from './components/user-root/user-root.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, UserRootComponent, UserFormComponent, UserFormComponent],
   providers: [],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule,],
   exports: [HomeComponent],
 })
 export class CustomerModule {}
---
+++ src/app/modules/customer/models/role.ts
@@ -0,0 +1,5 @@
+export class Role {
+    id_role: number;
+    name: string;
+    area: string
+}
