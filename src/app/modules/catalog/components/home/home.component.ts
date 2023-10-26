26382954154cafb40c37ce8939f37020b8b7e2b4
112866-JulianMartinez <91501018+JulianMartinezz@users.noreply.github.com>
2023-10-26 16:02:29 -0300

    Revert "Se agregar cargar boton listado de categorias en home de catalogo"
    This reverts commit ae00a060c6b5b526e615ecb0ea480a27c0cfe308.

--- src/app/modules/catalog/components/home/home.component.ts
+++ src/app/modules/catalog/components/home/home.component.ts
@@ -11,21 +11,18 @@ export class HomeComponent {
   addProductVisible: boolean = false;
   addDiscountVisible: boolean = false;
   discountsVisible: boolean = false;
-  categoryVisible: boolean = false;
 
   showAddProduct() {
     this.addProductVisible = true;
     this.productsVisible = false;
     this.addDiscountVisible = false;
     this.discountsVisible = false;
-    this.categoryVisible = false;
   }
   showProducts() {
     this.productsVisible = true;
     this.addProductVisible = false;
     this.discountsVisible = false;
     this.addDiscountVisible = false;
-    this.categoryVisible = false;
   }
 
   showAddDiscount() {
@@ -33,21 +30,11 @@ export class HomeComponent {
     this.discountsVisible = false;
     this.addProductVisible = false;
     this.productsVisible = false;
-    this.categoryVisible = false;
   }
   showDiscounts() {
     this.discountsVisible = true;
     this.addDiscountVisible = false;
     this.productsVisible = false;
     this.addProductVisible = false;
-    this.categoryVisible = false;
   }
-  showCategories() {
-    this.discountsVisible = false;
-    this.addDiscountVisible = false;
-    this.productsVisible = false;
-    this.addProductVisible = false;
-    this.categoryVisible = true;
-  }
-
 }
