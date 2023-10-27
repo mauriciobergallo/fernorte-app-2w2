4b3e9084e89acac1482f3cc9792d6be8375e5592
114051-SIRAVEGNA-MATIAS <114051@tecnicatura.frc.utn.edu.ar>
2023-10-27 10:46:51 -0300

    categories functionalities added

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -11,10 +11,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { TurnsComponentComponent } from './components/turns-component/turns-component.component';
 import { BtnNoCustomerComponent } from './components/btn-no-customer/btn-no-customer.component';
 import { BtnCustomerComponent } from './components/btn-customer/btn-customer.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CategoryService } from './services/category.service';
 
 @NgModule({
-  declarations: [HomeComponent, TurnsComponentComponent, BtnNoCustomerComponent, BtnCustomerComponent, EmployeeRegistrationComponent],
-  providers: [],
+  declarations: [HomeComponent, TurnsComponentComponent, BtnNoCustomerComponent, BtnCustomerComponent, EmployeeRegistrationComponent,CategoryComponent],
+  providers: [CategoryService],
   imports: [CommonModule, BrowserModule, ReactiveFormsModule, NgbModule, FormsModule, HttpClientModule],
   exports: [HomeComponent],
 })
