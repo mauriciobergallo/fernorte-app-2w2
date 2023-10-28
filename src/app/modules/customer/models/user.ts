b377af18b30034cc8fa7b905e526f36ddca32390
114042-TOLEDO-BRUNO <114042@tecnicatura.frc.utn.edu.ar>
2023-10-28 12:39:44 -0300

    conflict resolved

--- src/app/modules/customer/models/user.ts
+++ src/app/modules/customer/models/user.ts
@@ -2,13 +2,13 @@ import { Email } from "./email";
 import { Role } from "./role";
 
 export interface User {
-    id_user: number;
+    id_user?: number;
     first_login: boolean;
     password_reset: boolean;
     document_number: string;
-    email: Email;
+    email?: Email;
     username: string;
     user_password: string;
-    is_active: boolean;
+    is_active?: boolean;
     roles: Role[];
   }
