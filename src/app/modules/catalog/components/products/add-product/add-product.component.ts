f5fb49e3d75a974d9cab06c6308b18090a22b0f0
Samuel Risso <110845@tecnicatura.frc.utn.edu.ar>
2023-11-01 01:47:36 -0300

    Se agregan validaciones al formulario de alta de productos

--- src/app/modules/catalog/components/products/add-product/add-product.component.ts
+++ src/app/modules/catalog/components/products/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Optional } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ICategory } from '../../../models/ICategory';
@@ -14,62 +14,125 @@ import { ProductService } from '../../../services/product.service';
 })
 export class AddProductComponent {
   @Input() product?: IProductCategory | null = null;
-  listCategories:ICategory[] = [];
+  listCategories: ICategory[] = [];
   formGroup: FormGroup;
   isLoading: boolean = false;
 
-  image?:File
+  //Variables utilizadas para mostrar mensajes de validaciones
+  isSuccess: boolean = false;
+  successMessage: string = 'El producto se registró correctamente.';
+  showInvalidFormError: boolean = false;
+  invalidFormMessage: string = 'Por favor, completá todos los campos requeridos.'
 
-  constructor(private fb: FormBuilder, private prodService: ProductService, @Optional() private modalService: NgbActiveModal, private router: Router,private categoryService:CategoryService) {
+  predeterminatedCategoryId: number = 1;
+
+  image?: File
+
+  constructor(private fb: FormBuilder, private prodService: ProductService, @Optional() private modalService: NgbActiveModal, private router: Router, private categoryService: CategoryService) {
+    // Creación del formulario con validaciones
     this.formGroup = this.fb.group({
       id_product: [null],
-      name: [null],
+      name: [null, Validators.required],
       description: [null],
-      unit_price: [null],
-      stock_quantity: [null],
+      unit_price: [null, Validators.required],
+      stock_quantity: [null, Validators.required],
       unit_of_measure: [null],
-      id_category: [null],
+      id_category: [null, Validators.required],
       image: [null],
       user_created: [null]
     });
   }
 
   ngOnInit(): void {
+    // Obtener las categorías
     this.getCategories();
+    //Se setea una categoría por defecto al iniciar el formulario
+    this.formGroup.patchValue({ id_category: this.predeterminatedCategoryId });
   }
- getCategories(){
+  getCategories() {
     this.categoryService.get().subscribe((res) => {
       this.listCategories = res;
     });
- }
+  }
 
-  onFileSelected(event:any) {
-   this.image= event.target.files[0];
-   const fr = new FileReader();
-    fr.onload = (e:any) => {
+  onFileSelected(event: any) {
+     // Manejar la selección de archivos (imágenes)
+    this.image = event.target.files[0];
+    const fr = new FileReader();
+    fr.onload = (e: any) => {
       this.image = e.target.result;
     }
     fr.readAsDataURL(this.image as Blob);
   }
   onSubmit() {
-    this.isLoading = true;
-    let request = this.formGroup.value;
-    request.id_product = Number(request.id_product);
-    request.unit_price = Number(request.unit_price);
-    request.stock_quantity = Number(request.stock_quantity);
-    request.id_category = Number(request.id_category);
-    request.user_created = 'prueba';
-    request.image = this.image;
-    this.prodService.put(request).subscribe((res) => {
-      this.isLoading = false;
-      this.modalService.close(res);
-      this.router.navigateByUrl('/catalog/products/list', { skipLocationChange: true }).then(() => {
-      location.reload();
+    if (this.formGroup.valid) {
+      // El formulario es válido, procede con el envío
+      this.isLoading = true;
+      //Formula el request
+      let request = this.formGroup.value;
+      request.id_product = Number(request.id_product);
+      request.unit_price = Number(request.unit_price);
+      request.stock_quantity = Number(request.stock_quantity);
+      request.id_category = Number(request.id_category);
+      request.user_created = 'prueba';
+      request.image = this.image;
+
+      // Envía el request al servicio
+      this.prodService.put(request).subscribe((res) => {
+        // El producto se registró con éxito, muestra la alerta de éxito
+        this.showSuccessAlert();
+
+        // Simula una demora antes de cerrar el modal y redirigir para que la succesAlert sea visible
+        setTimeout(() => {
+          this.isLoading = false;
+          this.modalService.close(res);
+          this.router.navigateByUrl('/catalog/products/list', { skipLocationChange: true }).then(() => {
+            location.reload();
+          });
+        }, 3000);  // Espera 3 segundos (ajustar esto según preferencias)
       });
-    });
+    } else {
+      // El formulario no es válido entonces se muestra la alerta de error
+      this.showInvalidFormAlert();
+    }
   }
 
+
+
   close() {
+    // Cerrar el modal
     this.modalService.close(false);
+  }
+
+  // Métodos para obtener controles específicos del formulario
+  get controlName(): FormControl {
+    return this.formGroup.controls['name'] as FormControl;
+  }
+
+  get controlCategory(): FormControl {
+    return this.formGroup.controls['id_category'] as FormControl;
+  }
+  get controlUnitPrice(): FormControl {
+    return this.formGroup.controls['unit_price'] as FormControl;
+  }
+
+  get controlStockQuantity(): FormControl {
+    return this.formGroup.controls['stock_quantity'] as FormControl;
+  }
+
+// Método para mostrar la alerta de éxito
+  showSuccessAlert() {
+    this.isSuccess = true;
+    setTimeout(() => {
+      this.isSuccess = false;
+    }, 3000); // La alerta de oculta después de 3 segundos
+  }
+
+   // Método para mostrar la alerta de formulario inválido
+  showInvalidFormAlert() {
+    this.showInvalidFormError = true;
+    setTimeout(() => {
+      this.showInvalidFormError = false;
+    }, 3000); // La alerta de oculta después de 3 segundos
   }
 }
