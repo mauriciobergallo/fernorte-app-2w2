8d4532c51ef43bfc81e97f63c8b9edd4647beb05
matiob <buraschi.mateo@gmail.com>
2023-11-03 11:43:26 -0300

    payment-order-flow

--- src/app/modules/purchase/components/payment-order-container/payment-order-container.component.ts
+++ src/app/modules/purchase/components/payment-order-container/payment-order-container.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PaymentFlow } from '../../models/IPaymentOrder';
+import { PaymentOrderServiceService } from './services/payment-order-service.service';
 
 @Component({
   selector: 'fn-payment-order-container',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment-order-container.component.css']
 })
 export class PaymentOrderContainerComponent {
+
+  constructor(private _paymentOrderService: PaymentOrderServiceService){}
+
+  paymentOrderFlow: PaymentFlow = 'GRID';
+
+  ngOnInit(){
+    this._paymentOrderService.getPaymentOrderFlow().subscribe(flow => this.paymentOrderFlow = flow)
+  }
+
+  setPaymentOrderFlow(): void {
+    this.paymentOrderFlow === 'METHODS' 
+      ? this._paymentOrderService.setPaymentOrderFlow('PREVIEW') 
+      : this._paymentOrderService.setPaymentOrderFlow('METHODS')
+  }
 
 }
