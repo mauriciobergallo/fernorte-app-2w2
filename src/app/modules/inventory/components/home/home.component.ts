63b79b3ede8a41f9861caf8ff2f6296df2c00956
114255-BORDON-AXEL <114255@tecnicatura.frc.utn.edu.ar>
2023-11-03 16:44:01 -0300

    [Refactor] Unified branch work

--- src/app/modules/inventory/components/home/home.component.ts
+++ src/app/modules/inventory/components/home/home.component.ts
@@ -4,24 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'fn-home-inventory',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-
-  searchProductVisible : boolean = true;
-  searchMovementsVisible : boolean = false;
-
-  showSearchLocationProduct() : void {
-    this.searchProductVisible = true;
-    this.searchMovementsVisible = false;
-
-  }
-
-  showSearchMovements() : void {
-    this.searchMovementsVisible = true;
-    this.searchProductVisible = false;
-
-  }
-
-
-}
+export class HomeComponent {}
