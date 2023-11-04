5f76b34bcb393532ac2402c331d79a19582f974c
113213_Bogdan_Daniel <113213@tecnicatura.frc.utn.edu.ar>
2023-11-04 20:48:49 -0300

    open modal
    Missing the part where, when you open 'Add Role,' a modal should appear directly instead of a button.

--- src/app/modules/customer/components/role-list/role-list.component.ts
+++ src/app/modules/customer/components/role-list/role-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RoleService } from '../../services/role.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CreateRolComponent } from '../create-rol/create-rol.component';
 
 @Component({
   selector: 'fn-role-list',
@@ -7,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class RoleListComponent {
   show:boolean=false;
+  constructor(private modalService: NgbModal){}
+  openAdd(){
+    const modalRef = this.modalService.open(CreateRolComponent);
+  }
+    
 }
