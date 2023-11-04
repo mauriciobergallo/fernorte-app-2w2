e964ece6cf93fe840587a76cedde0589f541b7a3
112748_Ordoñez_David <112748@tecnicatura.frc.utn.edu.ar>
2023-11-04 16:58:20 -0300

    Merge, datepickerformat, validaiton in employee form

--- src/app/modules/customer/components/employee-registration/employee-registration.component.ts
+++ src/app/modules/customer/components/employee-registration/employee-registration.component.ts
@@ -1,90 +1,207 @@
-import { Employee } from './../../models/employee';
-import { Component } from '@angular/core';
-import { NgbDateStruct, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Employee } from '../../models/employee';
 import { EmployeeService } from '../../services/employee.service';
-import { NgForm, FormsModule } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+import { FormBuilder, FormGroup, FormsModule, Validators, AbstractControl  } from '@angular/forms';
 @Component({
-	selector: 'fn-employee-registration',
-	templateUrl: './employee-registration.component.html',
-	styleUrls: ['./employee-registration.component.css']
+  selector: 'fn-employee-registration',
+  templateUrl: './employee-registration.component.html',
+  styleUrls: ['./employee-registration.component.css']
 })
-export class EmployeeRegistrationComponent {
+export class EmployeeRegistrationComponent  {
+	
+	
+	minDate: NgbDateStruct = { year: 2000, month: 1, day: 1 };
+	maxDate: NgbDateStruct = { year: 2000, month: 1, day: 1 };;
+	currentYear = new Date().getFullYear();
 
-	//@ViewChild('employeeForm') employeeForm!: NgForm;
-	employeeForm!: NgForm;
-	formattedBirthDate: string = '';
+	maxLengthDocument : number = 0;
 
-	employee: Employee = {
-		firstName: "",
-		lastName: "",
-		birthDate: new Date(),
-		documentType: 1,
-		documentNumber: "",
-		address: "",
-		phoneNumber: "",
-		personalEmail: ""
-	};
+
+
+	employeeForm: FormGroup;
+	constructor(private modalService: NgbModal, private employeeService: EmployeeService, private fb: FormBuilder) {
+
+		this.employeeForm = this.fb.group({
+			firstName: ['', [Validators.required, Validators.pattern('^[^0-9]+$')]],
+			lastName: ['', [Validators.required, Validators.pattern('^[^0-9]+$')]],	
+			documentType: [0, Validators.required],
+			documentNumber: ['', Validators.required, Validators.maxLength(this.maxLengthDocument)],
+			birthDate:['', Validators.required],
+			personalEmail:['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}')]] ,
+			address:['', Validators.required],
+			phoneNumber:['', Validators.required]});
+
+
+    // Obtén la fecha actual
+    const currentDate = new Date();
+    // Resta 16 años de la fecha actual
+    const minYear = currentDate.getFullYear() - 16;
+    const minMonth = currentDate.getMonth() + 1; // Los meses en JavaScript son de 0 a 11, ng-bootstrap es de 1 a 12
+    const minDay = currentDate.getDate();
+
+    // Asigna la fecha calculada a minDate
+    this.minDate = { year: minYear, month: minMonth, day: minDay };
+
+
+    this.maxDate = {
+		year: currentDate.getFullYear() - 150, //Cambiar este numero si se quiere bajar la edad
+		month: currentDate.getMonth() + 1,
+		day: currentDate.getDate()
+	  };
+
+
+	}
+
+	@ViewChild('content') contentModal: any;
+
+employee: Employee = {
+firstName:"",
+lastName:"",
+birthDate: new Date(),
+documentType: 0,
+documentNumber:"",
+address:"",
+phoneNumber:"",
+personalEmail:""
+
+};
 	closeResult = '';
+	nameTouched = false;
+	lastNameTouched = false;
+	phoneNumberTouched = false;
+	emailTouched = false;
+	documentPattern = '';
+	documentNumberTouched = false;
 
-	constructor(private modalService: NgbModal, private employeeService: EmployeeService) { }
+	get firstNameControl(): AbstractControl | null {
+		return this.employeeForm.get('firstName');
+	  }
+
+	  get lastNameControl(): AbstractControl | null {
+		return this.employeeForm.get('lastName');
+	  }
+
+	  get documentNumberControl(): AbstractControl | null {
+		return this.employeeForm.get('documentNumber');
+	  }
+
+	onDocumentTypeChange() {
+
+		console.log("FORM", this.employeeForm);
+
+
+		const documentNumberControl = this.employeeForm.get('documentNumber');
+
+		console.log("DOCUMENT NUMBER", documentNumberControl);
+		const selectedDocumentType = this.employeeForm.value.documentType;
+		// Restablecer el valor del campo de documento
+		this.employee.documentNumber = '';
+
+
+
+
+		
+		// Aplicar las reglas de formato según el tipo de documento
+		switch (Number(selectedDocumentType)) {
+		  case 2: // DNI
+			// Permite solo números
+			documentNumberControl?.setValidators([Validators.required, Validators.pattern('^[0-9]+$')]);
+			this.maxLengthDocument = 10;
+		//	this.documentPattern = '^[0-9]+$';
+			break;
+	
+		  case 3: // CUIT 
+		  case 4: // CUIL
+			// Permite números y formato XX-XXXXXXXX-X
+			documentNumberControl?.setValidators([Validators.required, Validators.pattern('^[0-9]{2}-[0-9]{8}-[0-9]$')]);
+			this.maxLengthDocument = 12;
+			break;
+	
+		  case 5: // LC
+		  case 6: // LE
+			// Permite solo números
+			documentNumberControl?.setValidators([Validators.required, Validators.pattern('^[0-9]+$')]);
+			this.maxLengthDocument = 12;
+			break;
+	
+		  case 7: // Pasaporte
+			// Permite letras y números en formato AAA-000000
+			documentNumberControl?.setValidators([Validators.required, Validators.pattern( '^[A-Za-z]{3}-[0-9]{6}$')]);
+			this.maxLengthDocument = 10;
+			break;
+	
+		  default:
+			// Para otras opciones o cuando se restablezca a "Seleccione una opción"
+			this.documentPattern = '';
+			break;
+		}
+	  }
+
+	  onDocumentNumberInput() {
+		if (Number(this.employee.documentType) === 3 || Number(this.employee.documentType === 4)) {
+		  // Aplicar el formato XX-XXXXXXXX-X mientras el usuario escribe
+		  const input = this.employee.documentNumber.replace(/\D/g, '').substring(0, 11);
+		  this.employee.documentNumber = input.replace(/^(\d{2})(\d{8})(\d{1})$/, '$1-$2-$3');
+		}
+	  }
+
+	  
+	getDocumentNumberErrorMessage() {
+		switch (Number(this.employee.documentType)) {
+		case 2:
+			this.documentNumberTouched = false;
+			return 'Ingresa solo números para DNI.';
+		case 5:
+		case 6:
+			this.documentNumberTouched = false;
+			return 'Ingresa solo números para LC o LE.';
+		case 3:
+		case 4:
+			this.documentNumberTouched = false;
+			return 'Ingresa un formato válido (00-00000000-0).';
+		case 7:
+			this.documentNumberTouched = false;
+			return 'Ingresa un formato válido (AAA-000000).';
+		default:
+			this.documentNumberTouched = false;
+			return 'Ingresa un formato válido.';
+		}
+	}
+		
+
 
 	open(content: any) {
+		console.log("THIS", this);
+		console.log("content", content);
 		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
 			(result) => {
 
 				console.log("CONTENT", content);
 				console.log("RESULT", result);
-				console.log("EMPLOYEE FORM", this.employeeForm);
-
-				let newemployee: any = {
-					firstName: this.employee.firstName,
-					lastName: this.employee.lastName,
-					birthDate: this.formattedBirthDate,
-					documentType: this.employee.documentType,
-					address: this.employee.address,
-					phoneNumber: this.employee.phoneNumber,
-					documentNumber: this.employee.documentNumber,
-					personalEmail: this.employee.personalEmail
-				}
-				this.employeeService.postEmployee(newemployee).subscribe(
+				console.log("EMPLOYEE FORM",this.employeeForm);
 
 
-					(response) => {
-						alert("Se creo el empleado")
-					},
-					(error) => {
-						alert("Error en el servidor")
-					}
-				)
+				this.employeeService.createEmployee(this.employee);
 				this.employeeService.clearFields(this.employee);
 				this.closeResult = `Closed with: ${result}`;
+				this.employeeForm.reset();
+
 			},
 			(reason) => {
-
+				
 			},
 		);
 	}
 
-	onBirthDateChange(event: NgbDateStruct) {
-		if (event) {
-			const year = event.year || 0;
-			const month = event.month || 1;
-			const day = event.day || 1;
 
-			const selectedDate = new Date(year, month - 1, day);
 
-			this.formattedBirthDate = selectedDate.toISOString();
-		} else {
-			this.formattedBirthDate = '';
-		}
+
+	onSubmitForm(employeeForm: FormGroup){
+		console.log("EMPLOYEEEE", employeeForm);
 	}
 
-
-
-
-	onSubmitForm(employee: NgForm) {
-		console.log("employee", employee);
-	}
-
-
+  
 }
--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule, DatePipe } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { HomeComponent } from './components/home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateParserFormatter, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { EmployeeRegistrationComponent } from './components/employee-registration/employee-registration.component';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { TurnsComponentComponent } from './components/turns-component/turns-component.component';
 import { BtnNoCustomerComponent } from './components/btn-no-customer/btn-no-customer.component';
 import { BtnCustomerComponent } from './components/btn-customer/btn-customer.component';
-
 import { LoginComponent } from './components/login/login.component';
 import { EmployeeService } from './services/employee.service';
 import { RoleService } from './services/role.service';
@@ -21,10 +19,22 @@ import { CreateCustomerComponent } from './components/create-customer/create-cus
 import { CustomerService } from './services/customer.service';
 import { CreateRolComponent } from './components/create-rol/create-rol.component';
 import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
-
-@NgModule({
-  declarations: [HomeComponent, TurnsComponentComponent, BtnNoCustomerComponent, BtnCustomerComponent, EmployeeRegistrationComponent, LoginComponent, CreateCustomerComponent, CreateRolComponent, UpdateCustomerComponent],
-  providers: [EmployeeService, RoleService, TurnService, LoginService, CustomerService, DatePipe],
-
 import { CategoryComponent } from './components/category/category.component';
 import { CategoryService } from './services/category.service';
+import { CustomDateParserFormatter } from './components/shared/custom-date-formatter';
+
+
+
+
+
+@NgModule({
+  declarations: [HomeComponent, TurnsComponentComponent, BtnNoCustomerComponent, BtnCustomerComponent, EmployeeRegistrationComponent,CategoryComponent,
+    LoginComponent, CreateCustomerComponent, CreateRolComponent, UpdateCustomerComponent],
+  providers: [CategoryService,  
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter }, EmployeeService,
+    RoleService, TurnService, LoginService, CustomerService],
+  imports: [CommonModule, BrowserModule, ReactiveFormsModule, 
+    NgbModule, FormsModule, HttpClientModule, ReactiveFormsModule],
+  exports: [HomeComponent],
+})
+export class CustomerModule {}
