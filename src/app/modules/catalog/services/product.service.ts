6da171bbea9bb229ee7fc63a4e8f9fc537309ebd
112791-UlloaMariano <112791@tecnicatura.frc.utn.edu.ar>
2023-11-07 10:38:15 -0300

    Se modifica servicio de productos.

--- src/app/modules/catalog/services/product.service.ts
+++ src/app/modules/catalog/services/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
-import {  IProductCategory } from "../models/IProductCategory";
+import { IProductCategory } from "../models/IProductCategory";
 import { Observable } from "rxjs";
 import { environment } from "../environments/environment";
 import { RequestResponseService } from "./requestResponse.service";
 import { IProduct } from "../models/IProduct";
+import { HttpParams } from "@angular/common/http";
 
 @Injectable({
    providedIn: 'root'
@@ -14,8 +15,28 @@ export class ProductService {
 
    constructor(private requestResponseService: RequestResponseService) { }
 
-   get(): Observable<IProductCategory[]> {
-      return this.requestResponseService.makeGetRequest<IProductCategory[]>(this.products);
+   get(page?: number, size?: number, sortBy?: string, sortDir?: string, isDeleted?: boolean): Observable<IProductCategory[]> {
+      let params = new HttpParams();
+      if (page) {
+         params = params.set('page', page.toString());
+      }
+
+      if (size) {
+         params = params.set('size', size.toString());
+      }
+
+      if (sortBy) {
+         params = params.set('sortBy', sortBy);
+      }
+
+      if (sortDir) {
+         params = params.set('sortDir', sortDir);
+      }
+
+      if (isDeleted !== undefined) {
+         params = params.set('isDeleted', isDeleted.toString());
+      }
+      return this.requestResponseService.makeGetRequest<IProductCategory[]>(this.products, {params: params});
    }
    getById(id: number): Observable<IProductCategory> {
       return this.requestResponseService.makeGetRequest<IProduct>(`${this.products}/${id}`);
@@ -23,10 +44,10 @@ export class ProductService {
    getProductsByCategory(id: number): Observable<IProductCategory[]> {
       return this.requestResponseService.makeGetRequest<IProductCategory[]>(`${this.categories}/${id}`);
    }
-   put(product:IProduct): Observable<IProduct> {
+   put(product: IProduct): Observable<IProduct> {
       return this.requestResponseService.makePutRequest<IProduct>(this.products, product);
    }
-   delete(id:number , username:string): Observable<any> {
+   delete(id: number, username: string): Observable<any> {
       return this.requestResponseService.makeDeleteRequest<any>(this.products + "/" + id + "?username=" + username);
    }
 }
