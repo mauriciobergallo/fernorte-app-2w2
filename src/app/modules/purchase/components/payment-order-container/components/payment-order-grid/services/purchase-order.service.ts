4a56f2e14884c94f6c833110e3d196f5cd21b925
113898-KUMIEC <113898@tecnicatura.utn.frc.edu.ar>
2023-11-07 21:49:40 -0300

    commit all grid

--- src/app/modules/purchase/components/payment-order-container/components/payment-order-grid/services/purchase-order.service.ts
+++ src/app/modules/purchase/components/payment-order-container/components/payment-order-grid/services/purchase-order.service.ts
@@ -8,24 +8,45 @@ import { map } from 'rxjs/operators';
 })
 export class PurchaseOrderService {
 
-  url: string = 'http://localhost:8080/purchaseOrder/';
+  url: string = 'http://localhost:8084/purchaseOrder/';
 
   constructor(private _http: HttpClient) {}
 
+  // Propiedad para almacenar las órdenes de compra seleccionadas
+  selectedPurchaseOrders: IPurchaseOrder[] = [];
+
+  //Trae las ordenes de compra
   getPurchaseOrder(): Observable<IPurchaseOrder[]> {
     return this._http.get<IPurchaseOrder[]>(this.url);
   }
-  
-  getPurchaseOrderTotals(): Observable<IPurchaseOrder[]> {
-    return this._http.get<IPurchaseOrder[]>(this.url);
-  }
-  getUnpaidPurchaseOrders(): Observable<IPurchaseOrder[]> {
+  //Trae las ordenes de compra que esten aceptadas y por id... de cuando seleccionas el combobox
+  getUnpaidPurchaseOrdersBySupplier(supplierId: number): Observable<IPurchaseOrder[]> {
     return this._http.get<IPurchaseOrder[]>(this.url)
       .pipe(
         map((purchaseOrders: IPurchaseOrder[]) => {
-          console.log(purchaseOrders)
-          return purchaseOrders.filter(order => order.purchaseStatus==="ACCEPTED");
+          console.log(supplierId);
+          return purchaseOrders.filter(order => order.purchaseStatus === "ACCEPTED" && order.supplierId.toString() === supplierId.toString());
         })
       );
   }
+  //Trae las ordenes de compra que esten aceptadas. //por si la quieren usar
+  getUnpaidPurchaseOrders(supplierId: number): Observable<IPurchaseOrder[]> {
+    return this._http.get<IPurchaseOrder[]>(this.url)
+      .pipe(
+        map((purchaseOrders: IPurchaseOrder[]) => {
+          console.log(supplierId);
+          return purchaseOrders.filter(order => order.purchaseStatus === "ACCEPTED");
+        })
+      );
+  }
+    // Agregar una orden de compra a la lista de seleccionadas
+    addSelectedPurchaseOrder(order: IPurchaseOrder) {
+      this.selectedPurchaseOrders.push(order);
+    }
+  
+    // obtiene las ordenes de compra seleccionadas para pagar. esto deberian usar me parece el resto que utilice estas ordenes
+    getSelectedPurchaseOrders(): IPurchaseOrder[] {
+      return this.selectedPurchaseOrders;
+    }
+  
 }
