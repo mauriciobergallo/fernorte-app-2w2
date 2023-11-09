4e0ac9e387c2524d76de5577ff0cfdaa5a1a0cd3
113828-Samir <113828@tecnicatura.frc.utn.edu.ar>
2023-11-09 16:21:38 -0300

    refactorMovement

--- src/app/modules/inventory/services/movements-service/movements.service.ts
+++ src/app/modules/inventory/services/movements-service/movements.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IMovementDto } from '../../models/IMovementDto';
+import { Pagination } from '../../models/pagination';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovementsService {
   constructor(private http: HttpClient) {}
+  private  baseUrl = 'http://localhost:8083/movements';
 
   getAllMovements(): Observable<IMovementDto[]> {
-    const baseUrl = 'http://localhost:8083/movements';
-    return this.http.get<IMovementDto[]>(baseUrl);
+    return this.http.get<IMovementDto[]>(this.baseUrl);
+  }
+
+  getPaginationMovements(currentPage: number) : Observable<Pagination> {
+    return this.http.get<Pagination>(this.baseUrl+"/page/"+currentPage)
   }
 }
