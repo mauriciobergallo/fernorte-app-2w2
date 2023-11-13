7b17465a356bed567c3924be84f2d2740620093f
LucasAngelMeli <lucas.angel@mercadolibre.com>
2023-11-13 17:02:57 -0300

    feature add bill updated

--- src/app/modules/sales/components/billing/billing.component.ts
+++ src/app/modules/sales/components/billing/billing.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {BillServiceService} from "../../services/billing/bill-service.service";
+import {PaymentMethodService} from "../../services/payment-method.service";
+import {IPaymentMethod} from "../../interfaces/ipayment-method";
+import {SaleOrderServiceService} from "../../services/salesOrder/sale-order-service.service";
 declare var window: any;
 
 @Component({
@@ -9,16 +12,22 @@ declare var window: any;
   styleUrls: ['./billing.component.css']
 })
 export class BillingComponent {
-  productList: any[] = [];
-  paymentList: any[] = [];
-  orderId: number = 0;
+  paymentMethods: any[] = [];
+  paymentList: any = [];
+  amount: number | null = null;
+  orderId: number | null = null;
   paymentModal: any;
-  selectedPaymentMethod: any = "";
+  selectedPaymentMethod: any = -1;
+  order: any = {};
 
-  constructor(private billService: BillServiceService) {
+  constructor(private billService: BillServiceService, private paymentMethodService: PaymentMethodService, private saleOrderService: SaleOrderServiceService) {
   }
 
   ngOnInit(): void {
+   this.paymentMethodService.getPaymentMethods().subscribe(
+     (methods: any[]) =>
+       this.paymentMethods = methods
+   );
     this.paymentModal = new window.bootstrap.Modal(
       document.getElementById('paymentModal')
     );
@@ -29,7 +38,6 @@ export class BillingComponent {
   }
   finishPayment() {
     // confirm or save something
-    console.log(this.selectedPaymentMethod);
     alert("pagado");
     this.paymentModal.hide();
   }
@@ -42,8 +50,8 @@ export class BillingComponent {
 
   }
 
-  checkOrder(id: number) {
-    if (!this.billService.checkOrder(id)) {
+  checkOrder() {
+    if (this.orderId == null || !this.billService.checkOrder(Number(this.orderId))) {
       alert("invalidOrder")
       return
     }
@@ -52,6 +60,26 @@ export class BillingComponent {
 
 
   addPayment() {
-
+    if (this.selectedPaymentMethod != -1){
+      this.paymentList.push({
+        paymentMethod:this.selectedPaymentMethod,
+        amount:this.amount
+      });
+      console.log(this.selectedPaymentMethod);
+      this.selectedPaymentMethod = -1;
+      this.amount = null;
+    }
   }
+
+  searchBill() {
+    //if (this.orderId != null){
+      //this.saleOrderService.getSaleOrder(this.orderId).subscribe((order) => {
+        //this.order = order;
+        //console.log(this.order);
+       // }
+     // )
+    //}
+  }
+
+  protected readonly Number = Number;
 }
