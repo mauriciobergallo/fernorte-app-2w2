5077ab94ecb4dd30fad6833cae69b27e0c5790d2
luis ivetta <114588@tecnicatura.frc.utn.edu.ar>
2023-11-14 18:54:11 -0300

    list component

---
+++ src/app/modules/purchase/components/purchase-order-container/components/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fn-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+}
