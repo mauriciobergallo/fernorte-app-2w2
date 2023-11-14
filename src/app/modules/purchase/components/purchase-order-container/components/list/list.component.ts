b6f7b9b5bdd5dd058a9335899d7028bc3a3e2254
hks-maurice <tomas.corsico@hks.ar>
2023-11-14 20:42:51 -0300

    Added list of pruchaseOrders

--- src/app/modules/purchase/components/purchase-order-container/components/list/list.component.ts
+++ src/app/modules/purchase/components/purchase-order-container/components/list/list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PurchaseOrderBack } from 'src/app/modules/purchase/models/IPurchaseOrder';
+import { PurchaseOrderServiceService } from '../../services/purchase-order-service.service';
 
 @Component({
   selector: 'fn-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  listOrders:PurchaseOrderBack[] = [];
+
+  constructor(private servicePurchase:PurchaseOrderServiceService){
+
+  }
+
+  ngOnInit(): void {
+    this.listOrders = this.servicePurchase.GetListMockPurchase();
+  }
 
 }
