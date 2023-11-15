32d8caab1d2446dd2c68a6e9e7bd2613c60e6a95
luis ivetta <114588@tecnicatura.frc.utn.edu.ar>
2023-11-14 21:04:54 -0300

    buscador orden de compras

--- src/app/modules/purchase/components/purchase-order-container/components/list/list.component.ts
+++ src/app/modules/purchase/components/purchase-order-container/components/list/list.component.ts
@@ -10,6 +10,7 @@ import { PurchaseOrderServiceService } from '../../services/purchase-order-servi
 })
 export class ListComponent implements OnInit {
   listOrders:PurchaseOrderBack[] = [];
+  listOrdersFiltered:PurchaseOrderBack[] = [];
 
   constructor(private servicePurchase:PurchaseOrderServiceService){
 
@@ -18,5 +19,18 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     this.listOrders = this.servicePurchase.GetListMockPurchase();
   }
+
+  filterPurchaseOrder(value: string){
+    this.listOrdersFiltered = this.listOrders.filter((purchaseOrder) => 
+    {
+      return  (
+        purchaseOrder.supplierName.toLowerCase().includes(value.toLowerCase()) ||
+        purchaseOrder.employeeName.toLowerCase().includes(value.toLowerCase()) ||
+        purchaseOrder.observation.toLowerCase().includes(value.toLowerCase()) ||
+        purchaseOrder.purchaseStatus.toLowerCase().includes(value.toLowerCase()) ||
+        purchaseOrder.total.toString().includes(value)
+      );
+    }
+  )}
 
 }
