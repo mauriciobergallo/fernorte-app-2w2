a01bdc027587d87e808d244e725b864f9feda470
Bruno Emanuel Toledo <128556461+114042-TOLEDO-BRUNO@users.noreply.github.com>
2023-11-17 11:16:48 -0300

    Add integration new Role in Role-list OK

--- src/app/modules/customer/components/create-rol/create-rol.component.ts
+++ src/app/modules/customer/components/create-rol/create-rol.component.ts
@@ -1,46 +1,103 @@
-import { Component} from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Role } from '../../models/role';
-import { NgForm, FormsModule } from '@angular/forms';
+import { NgForm, FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { RoleService } from '../../services/role.service';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'fn-create-rol',
-  templateUrl: './create-rol.component.html',
-  styleUrls: ['./create-rol.component.css']
+	selector: 'fn-create-rol',
+	templateUrl: './create-rol.component.html',
+	styleUrls: ['./create-rol.component.css']
 })
-export class CreateRolComponent {
-  role: Role = {
-    name: "",
-    area: "",
-  }
+export class CreateRolComponent implements OnInit {
+	role: Role = {
+		name: "",
+		area: "",
+	}
 
-  roleForm!: NgForm;
+	roleForm!: FormGroup;
+	contentRole: any;
 
-  constructor(private modalService: NgbModal, private roleService: RoleService) {}
+	constructor(private modalService: NgbModal,
+		private roleService: RoleService,
+		private formBuilder:FormBuilder) { 
+			this.roleForm = this.formBuilder.group({
+				name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+				area: ['', Validators.required]
+			  });
+		}
 
-  open(content: any) {
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', backdrop: 'static'  }).result.then(
-			(result) => {
+	ngOnInit() {
 
-				console.log("CONTENT", content);
-				console.log("RESULT", result);
-				console.log("ROLE FORM",this.roleService);
+	}
 
-				this.roleService.createRole(this.role).subscribe({
-					next:(newRole: Role) =>{
-						alert("Rol: "+ newRole.name + " Creado correctamente")//Esto despues hay que hacerlo con sweet alert
-					},error:(error) => {
-						alert('Error al comunicarse con la API');//Esto despues hay que hacerlo con sweet alert y aca hay que poner que tiene que avisar cuando el rol ya existe en la DB
-					  }
-				})
-				this.roleService.clearFields(this.role);
-				
+	closeForm() {
+		this.modalService.dismissAll();
+	  }
 
+	  showConfirmation() {
+		// Utiliza SweetAlert o el método que prefieras para mostrar una confirmación
+		Swal.fire({
+		  title: '¿Estás seguro?',
+		  text: '¿Quieres agregar un nuevo rol?',
+		  icon: 'question',
+		  showCancelButton: true,
+		  confirmButtonText: 'Sí, agregar',
+		  cancelButtonText: 'Cancelar'
+		}).then((result) => {
+		  if (result.isConfirmed) {
+			// Lógica para agregar el rol
+			this.onSubmitForm();
+		  }
+		});
+	  }
+	
+	  showCancelConfirmation() {
+		Swal.fire({
+		  title: '¿Está seguro?',
+		  text: 'Si cancela, perderá los datos ingresados. ¿Desea continuar?',
+		  icon: 'warning',
+		  showCancelButton: true,
+		  confirmButtonText: 'Sí, cancelar',
+		  cancelButtonText: 'No, seguir editando'
+		}).then((result) => {
+		  if (result.isConfirmed) {
+			// Acción a realizar si el usuario confirma la cancelación
+			this.closeForm();
+		  }
+		});
+	  }
+
+
+	open(content: any) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', backdrop: 'static' }).result.then(
+			(result: Role) => {
+				if (result) {
+					this.modalService.dismissAll();
+				}
 			},
 			(reason) => {
-
-			},
+				this.modalService.dismissAll();
+			}
 		);
 	}
+
+	onSubmitForm() {
+		if (this.roleForm.valid) {
+		  this.roleService.createRole(this.roleForm.value).subscribe(
+			(newRole: Role) => {
+			  this.modalService.dismissAll(newRole);
+			  console.log(this.roleForm.value);
+			  Swal.fire('Éxito', 'Rol creado correctamente', 'success');
+			  this.roleService.notifyRolesUpdated();
+			},
+			(error) => {
+			  Swal.fire('Error', 'No se pudo crear el rol', 'error');
+			}
+		  );
+		} else {
+		  // El formulario no es válido, puedes mostrar un mensaje de error o hacer algo más
+		}
+	  }
 }
