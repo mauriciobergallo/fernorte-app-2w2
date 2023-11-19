efd1c72c1cbb640ea0384aa5fd87548f7cc91f23
114184-SUPPO-IMANOL <114184@tecnicatura.frc.utn.edu.ar>
2023-11-19 18:37:11 -0300

    implement login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { LoginService } from './modules/customer/services/login.service';
 
 @Component({
   selector: 'fn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private auth: LoginService) {}
+
+  isLogged(){
+    return this.auth.isLogged();
+  }
+
+  getRole(){
+    return this.auth.getRole();
+  }
+}
