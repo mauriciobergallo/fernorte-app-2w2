32623a40df3079fad0261b13a246657cfc2c35f3
114255-BORDON-AXEL <114255@tecnicatura.frc.utn.edu.ar>
2023-11-22 18:12:32 -0300

    Cambios

---
+++ src/app/modules/inventory/models/receptions-orders/GetPaginatedData.ts
@@ -0,0 +1,6 @@
+export interface GetPaginatedData {
+  page: number;
+  confirmed_orders_only: boolean;
+  filter_type: string;
+  filter_value: string;
+}
--- src/app/modules/inventory/services/booking-service/booking-service.service.ts
+++ src/app/modules/inventory/services/booking-service/booking-service.service.ts
@@ -6,6 +6,7 @@ import { IUpdateReceptionOrder } from '../../models/receptions-orders/UpdateRece
 import { IUpdatedOrderDto } from '../../models/receptions-orders/UpdatedOrderDto.interface';
 import { IUpdatedDetailDto } from '../../models/receptions-orders/updatedDetailDto.interface';
 import { Pagination } from '../../models/pagination';
+import { GetPaginatedData } from '../../models/receptions-orders/GetPaginatedData';
 
 @Injectable({
   providedIn: 'root',
@@ -27,34 +28,8 @@ export class BookingServiceService {
       this.baseUrl + '/' + orderId + '/details'
     );
   }
-  getFilteredReceptionOrders(
-    page: number,
-    confirmedOrderOnly: boolean,
-    filterType: string,
-    filterValue: string
-  ): Observable<Pagination> {
-    if (filterValue === '' || filterType === '') {
-      return this.http.get<Pagination>(
-        this.baseUrl +
-          '/paginated?page=' +
-          page +
-          '&confirmed_orders_only=' +
-          confirmedOrderOnly
-      );
-    } else {
-      console.log('SERVICIO FILTRADO');
-      return this.http.get<Pagination>(
-        this.baseUrl +
-          '/paginated?page=' +
-          page +
-          '&confirmed_orders_only=' +
-          confirmedOrderOnly +
-          '&filter_type=' +
-          filterType +
-          '&filter_value=' +
-          filterValue
-      );
-    }
+  getFilteredReceptionOrders(data: GetPaginatedData): Observable<Pagination> {
+    return this.http.post<Pagination>(this.baseUrl + '/paginated', data);
   }
   updateReceptionOrder(
     orderData: IUpdateReceptionOrder
